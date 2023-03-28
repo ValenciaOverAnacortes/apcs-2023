9aaafd3b398a463643fbb57f5a6922968d19aac0
ValenciaOverAnacortes <127899098+ValenciaOverAnacortes@users.noreply.github.com>
2023-03-28 21:20:19 +0000

    marc filiberto linares

--- unit10/Piday.java
+++ unit10/Piday.java
@@ -7,26 +7,22 @@ public class Piday{
     //public boolean bool = true;
     //private int count = 0;
     public static void main(String[] args) {
-        System.out.print(pi(0.0, true, 1));
+        System.out.print(pi(false, 1));
     }
 
-    public static double pi(double n, boolean bool, int div){
-        if(div < 100){
-            if(bool == false){
-                n = n - (4.0/(div));
-                div = div + 2;
-                bool = true;
-                pi(n, bool, div);
-                
-            }
-    
-            if(bool == true){
-                n = n + (4.0/(div));
-                div = div + 2;
-                bool = false;
-                pi(n, bool, div);
-            }
+    public static double pi(boolean bool, int div){
+        if(div >= 10000){
+            return 0.0;
         }
-        return n;
+        else{
+            
+            if(bool== true){
+                return -4.0/div + pi(false, div+2);
+            }
+            if (bool == false){
+                return 4.0/div + pi(true, div+2);
+            }
     }
+    return 0;
+}
 }
